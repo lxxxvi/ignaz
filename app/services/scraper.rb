17dddd1a3a83199bc60344f397dcb8dfb8d7d76d
lxxxvi <lxxxvi@users.noreply.github.com>
2020-03-29 15:54:36 +0200

    add bugsnag and exception handling in scraper

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -11,9 +11,19 @@ class Scraper
   end
 
   def self.call
+    errors = []
+
     Scraper::Urls.all.each do |url|
       puts "Processing #{url}"
       Scraper::Loader.new(url).call
+    rescue Scraper::LoaderError => e
+      errors << Scraper::Error.new(url, e)
+    end
+
+    if errors.any?
+      puts "There were #{errors.count} errors"
+      Bugsnag.notify(errors.join("\n"))
+      exit 9
     end
   end
 
