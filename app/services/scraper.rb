a083c8080a24765e9b9bda50c910bd9aa54124de
lxxxvi <lxxxvi@users.noreply.github.com>
2020-09-14 10:42:22 +0200

    refactor Scraper to decrease Ruboco offenses

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -2,17 +2,22 @@ require 'net/http'
 require 'csv'
 
 class Scraper
-  def get(url)
-    self.class.get(url)
+  attr_reader :errors
+
+  def initialize
+    @errors = []
   end
 
-  def self.get(url)
-    Net::HTTP.get(URI(url))
+  def call
+    scrape_urls
+
+    NullGapsFiller.call
+    DeltasCalculator.call
   end
 
-  def self.call
-    errors = []
+  private
 
+  def scrape_urls
     CovidCase.transaction do
       CovidCase.connection.execute("DELETE FROM #{CovidCase.table_name}")
 
@@ -20,24 +25,8 @@ class Scraper
         Rails.logger.info "Processing #{url}"
         Scraper::Loader.new(url).call
       rescue Scraper::LoaderError => e
-        errors << Scraper::Error.new(url, e)
+        @errors << Scraper::Error.new(url, e)
       end
     end
-
-    NullGapsFiller.call
-    DeltasCalculator.call
-
-    if errors.any?
-      puts "There were #{errors.count} errors"
-      puts errors.join("\n")
-      Bugsnag.notify(errors.join("\n"))
-      exit 9
-    end
-  end
-
-  private
-
-  def read_csv(content)
-    CSV.new(content, headers: true)
   end
 end
--- app/services/scraper/loader.rb
+++ app/services/scraper/loader.rb
@@ -1,17 +1,9 @@
-class Scraper::Loader < Scraper
+class Scraper::Loader
   def initialize(url)
     @url = url
-
-    super
   end
 
   def call
-    load!
-  end
-
-  private
-
-  def load!
     read_csv(get(@url)).each do |row|
       open_zh_row = Scraper::OpenZhRow.new(row)
       covid_case = open_zh_row.to_covid_case
@@ -19,5 +11,15 @@ class Scraper::Loader < Scraper
     rescue StandardError => e
       raise Scraper::LoaderError.new(row, e)
     end
+  end
+
+  private
+
+  def get(url)
+    Net::HTTP.get(URI(url))
+  end
+
+  def read_csv(content)
+    CSV.new(content, headers: true)
   end
 end
